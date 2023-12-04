c4e2147bfd60c4ebf340845a497ede599dc883ce
Ilona <Ilonaverenich@tut.by>
2023-12-04 14:23:05 +0300

    done

---
+++ tasks/8kyu.QuarterOfTheYear.js
@@ -0,0 +1,19 @@
+const quarterOf = (month) => {
+    const months ={
+        1:1,
+        2:1,
+        3:1,
+        4:2,
+        5:2,
+        6:2,
+        7:3,
+        8:3,
+        9:3,
+        10:4,
+        11:4,
+        12:4,
+    }
+
+    return months[month]
+    
+  }
