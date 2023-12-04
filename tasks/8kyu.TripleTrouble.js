cdb5c3d6e9a62a72b4e809b07046e25dbc41f7d8
Ilona <Ilonaverenich@tut.by>
2023-12-04 18:01:36 +0300

    done Triple Trouble

---
+++ tasks/8kyu.TripleTrouble.js
@@ -0,0 +1,17 @@
+/* Triple Trouble
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. 
+Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+Note: You can expect all of the inputs to be the same length. */
+
+function tripleTrouble(one, two, three){
+    let arr = [];
+    for (let i =0; i<=one.length-1; i++ ){
+        arr.push(one[i])
+        arr.push(two[i])
+        arr.push(three[i])
+    }
+   return arr.join('')
+}
