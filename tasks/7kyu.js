46b7e937c82bad74f967040c68581d7b2e683682
Ilona <Ilonaverenich@tut.by>
2023-12-10 12:31:52 +0300

    done Remove the minimum

--- tasks/7kyu.js
+++
@@ -1,6 +0,0 @@
-function removeSmallest(numbers) {
-    let el =[...numbers].sort((a,b)=>a-b)[0];
-    return numbers.filter((e,i,arr)=>arr.indexOf(el) !== i)
-  }
-
-console.log(removeSmallest([5, 4, 2, 3, 4, 1, 5,1]))
