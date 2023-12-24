30ae69fe589f1d1e1fbbff3352749a1dfc3a44e1
Ilona <Ilonaverenich@tut.by>
2023-12-24 22:42:43 +0300

    update

--- tasks/6kyu.Array.diff.js
+++ tasks/6kyu.Array.diff.js
@@ -3,5 +3,4 @@ It should remove all values from list a, which are present in list b keeping the
 arrayDiff([1,2],[1]) == [2]
 If a value is present in b, all of its occurrences must be removed from the other:
 arrayDiff([1,2,2,2,3],[2]) == [1,3] */
-
 let  arrayDiff = (a, b) => a.filter((item ,i)=> ! b.includes(item)) 
